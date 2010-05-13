d6c73c1a4227612b0865a57d9484ccca2feae7cf
Kane Baccigalupi <baccigalupi@gmail.com>
2010-05-12 20:14:30 -0700

    Exchanging fixtures for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+require 'populator'
+
+Product.delete_all
+Product.populate 0..100 do |product|
+  product.title = Populator.words(1..2).titleize 
+  product.price = "#{rand(99)}.#{rand(99)}"
+  product.created_at = 2.years.ago..Time.now
+end 
